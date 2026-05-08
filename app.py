3b2139564c4a400f9ad3cb7713e3a371676edb32
Git1Mustafa <140718730+Git1Mustafa@users.noreply.github.com>
2026-05-08 19:36:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -232,8 +232,8 @@ def internal_error(error):
     db.session.rollback()
     return jsonify({"error":"Internal server error"}), 500
 
+create_tables()
 if __name__ == "__main__":
-    create_tables()
     print("🚀 Personal Finance Tracker Backend (SQLite)")
-    print("🔗 Frontend should connect to: http://127.0.0.1:5000")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
